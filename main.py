4fad87f0712944eac3a52eaa879c1fdf6f56febd
solat1993 <s.houshmand93@gmail.com>
2021-11-12 18:49:02 +0330

    add work names of every artist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import os
-
+import pprint
 
 class PathFinder:
     @staticmethod
@@ -9,8 +9,6 @@ class PathFinder:
 
 
 class HTMLReader(PathFinder):
-    def __init__(self):
-        pass
 
     @staticmethod
     def open_file(path):
@@ -28,17 +26,60 @@ class HTMLReader(PathFinder):
         return files
 
 
-class InformationGetter(HTMLReader):
-    def get_artist_name(self, directory):
+class InformationExtractor(HTMLReader):
+    def get_artist_names(self, directory):
         files = self.get_files(directory)
         artist_names = []
         for file in files:
-            artist_names.append(file.h2.text)
+            artist_names.append(self.get_artist_name(file))
         return artist_names
+
+    def artist_dict_builder(self, directory):
+
+        artist_names = self.get_artist_names(directory=directory)
+        artists_works_list = []
+
+        for artist in artist_names:
+            artists_dict = {}
+            artists_dict['artist'] = artist
+            artists_dict['works'] = self.get_works_of_an_artist(artist=artist, directory=directory)
+            artists_works_list.append(artists_dict)
+
+        print(artists_works_list)
+
+
+
+
+    @staticmethod
+    def get_artist_name(file):
+        return file.h2.text
+
+    @staticmethod
+    def get_work_name(file):
+        return file.h3.text
+
+    @staticmethod
+    def get_work_sale(file):
+        return file.findAll('div')[1].text
+
+    @staticmethod
+    def split_price_and_currency(sale_string):
+        return sale_string.split(" ")
+
+    def get_works_of_an_artist(self, artist, directory):
+        files = self.get_files(directory)
+        work_names = []
+        for file in files:
+            if artist == self.get_artist_name(file):
+                work_names.append(self.get_work_name(file))
+
+        return work_names
 
 
 if __name__ == '__main__':
-    parser_class = InformationGetter()
+    parser_class = InformationExtractor()
 
     path = 'data/2015-03-18/'
-    print(parser_class.get_artist_name(path))
+    #print(parser_class.get_works_of_an_artist('Rembrandt Harmensz. van Rijn', path))
+
+    pprint.pprint(parser_class.artist_dict_builder(path))
